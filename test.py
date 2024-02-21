b37df41ec2cd1c26c283f3a9921059f90b48f3b9
russell-myers1 <rbjmyers@gmail.com>
2024-02-20 17:10:10 -0800

    Initial commit with README and project files

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import requests
 
-def test_motd():
+def motd():
     response = requests.get('http://127.0.0.1:5000/motd')
     print('MOTD:', response.text)
 
-def test_version():
+def version():
     response = requests.get('http://127.0.0.1:5000/version')
     print('Version:', response.text)
 
 if __name__ == '__main__':
-    test_motd()
-    test_version()
+    motd()
+    version()
